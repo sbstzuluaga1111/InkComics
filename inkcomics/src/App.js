8f7fea90b95efe0d53844d0e13552bd5d29b23cb
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-09-19 07:25:47 -0500

    V2-React

--- inkcomics/src/App.js
+++ inkcomics/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import Home from './screens/Benefits';
+import Services from './screens/Services';
+import Contact from './screens/Contact'
+import Benefits from './screens/Benefits';
+import HowWeWork from './screens/HowWeWork';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Home />
     </div>
   );
 }
